712e05e6ce41b553891f40eaa1a3ff417a15cf48
Nora <nora.harris@daugherty.com>
2019-08-30 13:17:30 -0500

    connect to mongo

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 
 const app = express()
+
+//Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+  useNewUrlParser: true
+})
+.then(() => {console.log('MongoDB connected...')})
+.catch(err => console.log(err))
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
