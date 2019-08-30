bfb39d545c0c3fc10dce5ee168ab215b4f0de8b2
Nora <nora.harris@daugherty.com>
2019-08-30 12:20:49 -0500

    added middleware

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const express = require('express')
 
 const app = express()
 
+// How middleware works, how authentication will work. will put user information from session into request.user, access from anywhere
+app.use(function(req, res, next){
+  //console.log('Time:', Date.now())
+  req.name = 'Nora Harris'
+  next()
+})
+
 //Index Route
 app.get('/', (req, res) => {
+  console.log(req.name)
   res.send('index')
 })
 
+//About Route
 app.get('/about', (req, res) => {
   res.send('about')
 })
