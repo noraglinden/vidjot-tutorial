153bb754dbf2d8de8f9b4a653de2edd79cb21f79
Nora <nora.harris@daugherty.com>
2019-08-30 12:26:29 -0500

    added handlebars middleware

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 //Index Route
 app.get('/', (req, res) => {
