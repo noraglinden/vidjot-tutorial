611289dec6a6246d8510b08b759fced64910a390
Nora <nora.harris@daugherty.com>
2019-08-30 12:00:40 -0500

    started vidjot project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+//Index Route
+app.get('/', (req, res) => {
+  res.send('index')
+})
+
+const port = 5000
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+})
