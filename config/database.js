450c0b5c6a8712191bd035c380a5dc49ec22ca44
Nora <nora.harris@daugherty.com>
2019-09-02 18:21:26 -0500

    udpated db uri

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
-    mongoURI: 'mongodb://nora:nora@vidjot-prod-qotco.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb://nora:<password>@vidjot-prod-shard-00-00-qotco.mongodb.net:27017,vidjot-prod-shard-00-01-qotco.mongodb.net:27017,vidjot-prod-shard-00-02-qotco.mongodb.net:27017/test?ssl=true&replicaSet=vidjot-prod-shard-0&authSource=admin&retryWrites=true&w=majority'
   }
 } else {
   module.exports = {
