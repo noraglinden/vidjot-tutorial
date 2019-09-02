1768538687122f6f75b766ed19252a59a94355eb
Nora <nora.harris@daugherty.com>
2019-09-02 11:41:04 -0500

    edited navbar to show based on logged in or not

--- app.js
+++ app.js
@@ -42,6 +42,10 @@ app.use(session({
   saveUninitialized: true
 }))
 
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash())
 
 //global variables
@@ -49,6 +53,7 @@ app.use(function(req, res, next){
   res.locals.success_msg = req.flash('success_msg')
   res.locals.error_msg = req.flash('error_msg')
   res.locals.error = req.flash('error')
+  res.locals.user = req.user || null
   next()
 })
 
